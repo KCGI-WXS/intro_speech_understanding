06c99cc28c04158b766fb1529bfcdd25968ddafd
KCGI-WXS <146790556+KCGI-WXS@users.noreply.github.com>
2023-10-24 10:52:24 +0900

    homework3

--- 2023_fall/lec03/homework3.py
+++ 2023_fall/lec03/homework3.py
@@ -1,25 +1,26 @@
 
 
-def cancellation(list, stop_word):
-    '''
-    Copy elements one by one from input_list into output_list. 
-    If one of the elements is equal to the stop_word, then stop the function,
-    and return what you have so far.
-    '''
-    pass
+def cancellation(input_list, stop_word):
+    new_list = []
+    for i in input_list:
+        if i !=stop_word:
+            new_list.append(i)
+        else:
+            break
+    return new_list
+        
 
 def copy_all_but_skip_word(input_list, skip_word):
-    '''
-    This function should copy elements one by one from input_list into output_list.
-    If one of the elements is equal to the skip_word, then you should skip that element,
-    but keep checking all of the other elements.
-    '''
-    pass
+    output_list = []
+    for i in input_list:
+        if i != skip_word:
+            output_list.append(i)
+    return output_list
+
 
 def my_average(input_list):
-    '''
-    You may assume that `input_list` is a non-empty list, in which every element is a number.  
-    Calculate the average value, and return it. 
-    '''
-    pass
+    sum_input_list = 0
+    for i in input_list:
+        sum_input_list = sum_input_list + float(i)
+    return sum_input_list/len(input_list)
 
